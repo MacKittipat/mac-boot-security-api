ed72e88b198e90c6416187eb51a862d6b8820109
Mac <mac.kittipat@gmail.com>
2022-04-09 14:00:38 +0700

    Remove unnecessary code

--- mac-boot-security-api-client/src/main/java/com/mackittipat/macbootsecurityapiclient/config/SecurityConfig.java
+++ mac-boot-security-api-client/src/main/java/com/mackittipat/macbootsecurityapiclient/config/SecurityConfig.java
@@ -10,7 +10,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.oauth2Client();
-        http.authorizeRequests()
-                .anyRequest().permitAll();
     }
 }
